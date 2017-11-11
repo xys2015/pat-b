2c138398fee86eec2879508435c81d630faf25f3
Asheng <xys4k@qq.com>
2017-11-11 22:15:12 +0800

    1038太简单了, 核心代码不超过10行

---
+++ src/1038.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (void) {
+    int n; // 学生总数
+    int s; // 要查询的学生数
+    int nall[100001];
+    int sall[100001];
+    int score[101] = {0};
+    int temp;
+    int i;
+    char ch = ' ';
+
+    scanf("%d", &n);
+    for (i=1; i<=n; i++) {
+        scanf("%d", &temp);
+        score[temp]++;
+    }
+    scanf("%d", &s);
+    for (i=1; i<=s; i++) {
+        scanf("%d", &temp);
+        if (i == s) ch = '\n';
+        printf("%d%c", score[temp], ch);
+    }
+
+    return 0;
+}
