fc5b6feb3d9153e6658f47088e38f838ed52cfea
Asheng <xys4k@qq.com>
2017-11-12 15:20:10 +0800

    只通过了2个测试点, 想法不对

--- src/1040.c
+++ src/1040.c
@@ -19,8 +19,76 @@ APPAPT
 2
 */
 #include <stdio.h>
+#include <string.h>
 
 int main (void) {
+    long long int nump = 0; 
+    long long int numa = 0;
+    long long int numt = 0;
+    long long int numpat = 0;
+    long long int mod = 1000000007;
+    char str[100010];
+    int len;
+    int pi = 0;
+    int flag = 1; // 循环结束标志
+    int lastp = 0; // 最后一个P的位置
+    int curp;
+    int i;
+
+    scanf("%s", str);
+    len = strlen(str);
+    for (i=0; i<len; i++) {
+        if (str[i] == 'P') nump++;
+        if (str[i] == 'A') numa++;
+        if (str[i] == 'T') numt++;
+    }
+    if (nump != 0 && numa != 0 && numt != 0) {
+        flag = 1;
+    } else {
+        flag = 0;
+    }
+    // printf("%lld %lld %lld\n", nump, numa, numt);
+    if (flag == 1) {
+        for (i=len-1; i>=0; i--) {
+            if (str[i] == 'P') {
+                lastp = i;
+                break;
+            }
+        }        
+    }
+    while (flag == 1) {
+        nump = 0;
+        numa = 0;
+        numt = 0;
+        for (i=pi; i<len; i++) {
+            if (str[i] == 'A') {
+                pi = i + 1;
+                break;
+            }
+            if (str[i] == 'P') {
+                nump++;
+                curp = i;
+            }
+        }
+        for (; i<len; i++) {
+            if (str[i] == 'T') {
+                break;
+            }
+            if (str[i] == 'A') {
+                numa++;
+            }
+        }
+        for (; i<len; i++) {
+            if (str[i] == 'T') {
+                numt++;
+            }
+        }
+        if (i == len && curp == lastp) {
+            flag = 0;
+        }
+        numpat += nump * numa * numt;
+    }
+    printf("%lld\n", numpat % mod);
 
     return 0;
 }
