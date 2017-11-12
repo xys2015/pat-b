5f659037e63ea9346d5273b35d3787e0ee43df7e
Asheng <xys4k@qq.com>
2017-11-12 21:44:28 +0800

    hello-world

--- src/hello3.c
+++ src/hello3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
-int main (void) {
-    
+#define LIM 1000000007
+int main()
+{
+    unsigned int P = 0, PA = 0, PAT = 0;
+    char c;
+    while((c = getchar()) != '\n')
+    {
+        if(c == 'P')   P++;
+        if(c == 'A')   PA = (PA + P) % LIM;
+        if(c == 'T')   PAT = (PAT + PA) % LIM;
+    }
+    printf("%d\n", PAT);
     return 0;
 }
+
+// PAPA ATTP ATTT
+// PAPAATTPATTT  (34)
