f3a26e1c59ae6b133a28cbf5a9cbbb45abd27025
Asheng <xys4k@qq.com>
2017-11-12 18:38:54 +0800

    看了OliverLew大神的解法, 我想自己写的太渣了, 简直就是一坨翔, 根本不能看!!!
    推翻重写!!!!

--- src/1040.c
+++ src/1040.c
@@ -22,65 +22,73 @@ APPAPT
 #include <string.h>
 
 struct pat {
-    int num;
-    int pos;
+    int num; // 一共找到多少字符
+    int pos; // 从什么位置开始找
+    char cur; // 当前字符
+    char next; // 下一个字符
 };
 typedef struct pat s_pat;
 
-s_pat find (s_pat s, char search, char end, char str[]);
+s_pat find (s_pat s, char str[], int len);
 
 int main (void) {
-    s_pat p = {0 ,0};
-    s_pat a = {0 ,0};
-    s_pat t = {0 ,0};
+    s_pat p = {0, 0, 'P', 'A'};
+    s_pat a = {0, 0, 'A', 'T'};
+    s_pat t = {0, 0, 'T', '\0'};
     long long int mod = 1000000007;
     long long int total = 0;
     char str[100010];
     int len;
+    int flag;
     int i;
 
     scanf("%s", str);
     len = strlen(str);
-    // 把开头第一个'P'前面的东西拿掉
+    
     for (i=0; i<len; i++) {
-        if (str[i] == 'P') {
-            p.pos = i;
-            break;
-        }
+        if (str[i] == 'P') p.num++;
+        if (str[i] == 'A') a.num++;
+        if (str[i] == 'T') t.num++;
+    }
+    if (p.num != 0 && a.num != 0 && t.num != 0) {
+        flag = 1;
+    } else {
+        flag = 0;
     }
 
-    while (1) {
+    while (flag == 1) {
         p.num = 0;
         a.num = 0;
         t.num = 0;
-        p = find(p, 'P', 'A', str);
+        p = find(p, str, len);
         if (p.pos == len) break;
-        a.pos = p.pos - 1;
-        a = find(a, 'A', 'T', str);
+        a.pos = p.pos;
+        a = find(a, str, len);
         if (a.pos == len) break;
-        t.pos = a.pos - 1;
-        t = find(t, 'T', '\0', str);
-        printf("%d %d %d\n", p.num, a.num, t.num);
+        t.pos = a.pos;
+        t = find(t, str, len);
+        // printf("%d %d %d\n", p.num, a.num, t.num);
         if (p.num == 0 || a.num == 0 || t.num == 0) break;
         total += p.num * a.num * t.num;
     }
-    // printf("%lld\n", total % mod);
+    printf("%lld\n", total % mod);
     return 0;
 }
 
-s_pat find (s_pat s, char search, char end, char str[]) {
+s_pat find (s_pat s, char str[], int len) {
     int i;
-    int len = strlen(str);
     for (i=s.pos; i<len; i++) {
-        if (str[i] == end && s.num != 0) {
-            s.pos = i + 1;
+        if (str[i] == s.next && s.num != 0) {
+            s.pos = i;
             break;
         }
-        if (str[i] == search) {
+        if (str[i] == s.cur) {
             s.num++;
         }
     }
-    if (i == len) s.pos = len;
+    if (i == len) {
+        s.pos = len;
+    }
     return s;
 }
 // PAPAATTPATTT
