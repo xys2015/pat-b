4b2acfddb97ec58d1486ffd857d1ecff67730326
Asheng <xys4k@qq.com>
2017-11-12 21:44:12 +0800

    10几行代码就搞定的问题, 自己就是没想打, 还要练

--- src/1040.c
+++ src/1040.c
@@ -1,95 +1,16 @@
-/*
-字符串APPAPT中包含了两个单词“PAT”，其中
-第一个PAT是第2位(P),第4位(A),第6位(T)；
-第二个PAT是第3位(P),第4位(A),第6位(T)。
-
-现给定字符串，问一共可以形成多少个PAT？
-
-输入格式：
-
-输入只有一行，包含一个字符串，长度不超过10^5，只包含P、A、T三种字母。
-
-输出格式：
-
-在一行中输出给定字符串中包含多少个PAT。由于结果可能比较大，只输出对10 0000 0007取余数的结果。
-
-输入样例：
-APPAPT
-输出样例：
-2
-*/
 #include <stdio.h>
-#include <string.h>
-
-struct pat {
-    int num; // 一共找到多少字符
-    int pos; // 从什么位置开始找
-    char cur; // 当前字符
-    char next; // 下一个字符
-};
-typedef struct pat s_pat;
-
-s_pat find (s_pat s, char str[], int len);
-
+#define MOD 1000000007
 int main (void) {
-    s_pat p = {0, 0, 'P', 'A'};
-    s_pat a = {0, 0, 'A', 'T'};
-    s_pat t = {0, 0, 'T', '\0'};
-    long long int mod = 1000000007;
-    long long int total = 0;
-    char str[100010];
-    int len;
-    int flag;
-    int i;
+    long int p = 0;
+    long int pa = 0;
+    long int pat = 0;
+    char ch;
 
-    scanf("%s", str);
-    len = strlen(str);
-    
-    for (i=0; i<len; i++) {
-        if (str[i] == 'P') p.num++;
-        if (str[i] == 'A') a.num++;
-        if (str[i] == 'T') t.num++;
+    while ((ch = getchar()) != '\n') {
+        if (ch == 'P') p++;
+        if (ch == 'A') pa = (pa + p) % MOD;
+        if (ch == 'T') pat = (pat + pa) % MOD;
     }
-    if (p.num != 0 && a.num != 0 && t.num != 0) {
-        flag = 1;
-    } else {
-        flag = 0;
-    }
-
-    while (flag == 1) {
-        p.num = 0;
-        a.num = 0;
-        t.num = 0;
-        p = find(p, str, len);
-        if (p.pos == len) break;
-        a.pos = p.pos;
-        a = find(a, str, len);
-        if (a.pos == len) break;
-        t.pos = a.pos;
-        t = find(t, str, len);
-        // printf("%d %d %d\n", p.num, a.num, t.num);
-        if (p.num == 0 || a.num == 0 || t.num == 0) break;
-        total += p.num * a.num * t.num;
-    }
-    printf("%lld\n", total % mod);
+    printf("%ld", pat);
     return 0;
 }
-
-s_pat find (s_pat s, char str[], int len) {
-    int i;
-    for (i=s.pos; i<len; i++) {
-        if (str[i] == s.next && s.num != 0) {
-            s.pos = i;
-            break;
-        }
-        if (str[i] == s.cur) {
-            s.num++;
-        }
-    }
-    if (i == len) {
-        s.pos = len;
-    }
-    return s;
-}
-// PAPAATTPATTT
-// APPAPT
