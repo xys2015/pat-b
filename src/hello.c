4aafae6e241ae3728902d3d67e89614d7805b3d0
Asheng <xys4k@qq.com>
2017-11-14 18:26:03 +0800

    杂货提交

--- src/hello.c
+++ src/hello.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main (void) {
-    int i;
-    int k = 8;
-    int N = 14;
-    for( i=0; i<N/k; i++ ) {
-        printf("%d %d\n", k*i, k*i+k-1);
-    }
-    for( i=k*(N/k); i<N; i++ ) {
-        printf("%d\n", i); 
-    }
-    return 0;
+    // elo nov
+    char str[20] = "hello world";
+    char res1[20];
+    char res2[20];
+    int suc;
+
+    suc = sscanf(str, "%s %s", res1, res2);
+    printf("suc: %d\n", suc);
+    printf("res1: %s\n", res1);
+    printf("res2: %s\n", res2);
+    printf("r1len: %d\n", strlen(res1));
+    printf("r2len: %d\n", strlen(res2));
+    
+   return(0);
 }
