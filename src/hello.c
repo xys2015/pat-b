68d5a1426fd846e13f5206e2fc82a6e287437acd
Asheng <xys4k@qq.com>
2017-11-18 15:52:01 +0800

    hello提交

--- src/hello.c
+++ src/hello.c
@@ -1,19 +1,55 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main (void) {
-    // elo nov
-    char str[20] = "hello world";
-    char res1[20];
-    char res2[20];
-    int suc;
+int cmp(const void *a, const void *b) {return *(int*)b - *(int*)a;}
 
-    suc = sscanf(str, "%s %s", res1, res2);
-    printf("suc: %d\n", suc);
-    printf("res1: %s\n", res1);
-    printf("res2: %s\n", res2);
-    printf("r1len: %d\n", strlen(res1));
-    printf("r2len: %d\n", strlen(res2));
-    
-   return(0);
+int main()
+{
+    int N, m, n;
+    scanf("%d", &N);
+
+    int *array = (int*)malloc(N * sizeof(int));
+    int *matrix = (int*)malloc(N * sizeof(int));
+    for(int i = 0; i < N; i++)
+        scanf("%d", array + i);
+
+    qsort(array, N, sizeof(int), cmp);
+
+    /* determine m and n */
+    for(m = 1; !(m * m >= N && N % m == 0); m++) ;
+    n = N / m;
+
+    int x = -1, y = 0, index = 0;
+    int horizontal = n, virtical = m;
+        // w h
+    while(horizontal > 0 && virtical > 0)
+    {
+        for(int i = 0; i < horizontal && virtical > 0; i++)  /* toward right */
+            matrix[y * n + ++x] = array[index++];
+        virtical--;
+
+        for(int i = 0; i < virtical && horizontal > 0; i++)  /* toward bottom */
+            matrix[++y * n + x] = array[index++];
+        horizontal--;
+
+        for(int i = 0; i < horizontal && virtical > 0; i++)  /* toward left */
+            matrix[y * n + --x] = array[index++];
+        virtical--;
+
+        for(int i = 0; i < virtical && horizontal > 0; i++)  /* toward top */
+            matrix[--y * n + x] = array[index++];
+        horizontal--;
+    }
+
+    char ch;
+    for (int i = 0; i < index; i++) {
+        if ((i + 1) % n == 0) {
+            ch = '\n';
+        } else {
+            ch = ' ';
+        }
+        printf("%d%c", matrix[i], ch);
+    }
+
+    return 0;
 }
--- src/hello2.c
+++ src/hello2.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
-#include <string.h> 
- 
-int main (void)  {
-    int num = 100;
-    if (1) {
-        int num = 2;
-        printf("in.num: %d\n", num);
+#include <string.h>
+int main()
+{
+    char A[101], B[101];
+    char encrypt[] = "0123456789JQK";
+
+    scanf("%s %s", A, B);
+    int lenA = strlen(A);
+    int lenB = strlen(B);
+    int maxlen = lenA > lenB ? lenA : lenB;
+    int a, b;
+
+    for(int i = 0; i < maxlen; i++)
+    {
+        a = lenA + i - maxlen < 0 ? 0 : A[lenA + i - maxlen] - '0';
+        b = lenB + i - maxlen < 0 ? 0 : B[lenB + i - maxlen] - '0';
+        if((maxlen - i) % 2) {
+            putchar(encrypt[(a + b) % 13]); // 奇数
+        } else {
+            int res;
+            // putchar('0' + (b - a < 0 ? b - a + 10 : b - a));
+            res = b - a < 0 ? b - a + 10 : b - a; // 偶数
+            printf("%d", res);
+        }
     }
-    printf("out.num: %d\n", num);
 
     return 0;
 }
+
+/*
+正确: 
+56456487878 11212147788995542111
+123456789
+00000000056456487878
+11212147788995542111
+1121214770204Q1K5949
+
+11212147731519969383
+*/
