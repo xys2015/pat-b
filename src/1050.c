a16f716172af6d2d260709d759ba9e7f334be6cf
Asheng <xys4k@qq.com>
2017-11-18 15:51:48 +0800

    遍历矩形也没那么难

--- src/1050.c
+++ src/1050.c
@@ -1,92 +1,92 @@
-/*
-本题要求将给定的N个正整数按非递增(递减)的顺序，填入“螺旋矩阵”。所谓“螺旋矩阵”，是指从左上角第1个格子开始，按顺时针螺旋方向填充。要求矩阵的规模为 m行n列，
-满足条件：m*n等于N；m>=n； 行>=列
-且m-n取所有可能值中的最小值。
-
-输入格式：
-
-输入在第1行中给出一个正整数N，第2行给出N个待填充的正整数。所有数字不超过10^4，相邻数字以空格分隔。
-
-输出格式：
-
-输出螺旋矩阵。每行n个数字，共m行。相邻数字以1个空格分隔，行末不得有多余空格。
-
-输入样例：
-12
-37 76 20 98 76 42 53 95 60 81 58 93
-
-0  1  2  3  4  5  6  7  8  9  10 11
-98 95 93 81 76 76 60 58 53 42 37 20
-98 95 93 42 37 81 53 20 76 58 60 76
-0  1  2  9  10 3  8  11 4  7  6  5                    
-0  1  2  3  4  5  6  7  8  9  10 11
-输出样例： m=4行, n=3列
-98 95 93
-42 37 81
-53 20 76
-58 60 76
-*/
 #include <stdio.h>
+#include <stdlib.h> // 定义qsort
+#define LEN 10010
+
+int compare (const void *a, const void *b);
 
 int main (void) {
     int n;
-    int hang;
-    int lie;
-    int rhang;
-    int rlie;
-    int min;
-    int src[10010];
-    int matrix[10010];
-    int pos;
-    int temp;
+    int height;   // 高
+    int width;    // 宽
+    int src[LEN]; // 原始数组
     int i;
 
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
-        scanf("%d", src[i]);
+        scanf("%d", &src[i]);
     }
-    matrix = src[0]; // 起始位置
-    src[0] = 0; // 标记起点为0
-    rhang = n;
-    rlie = 1;
-    min = n - 1;
-    for (i = 1; i <= n / 2; i++) {
-        if (n % i == 0) {
-            hang = n / i;
-            lie = i;
-            if (hang < lie) { // 交换
-                temp = hang;
-                hang = lie;
-                lie = temp;
-            }
-            if (min > hang - lie) {
-                min = hang - lie;
-                rhang = hang;
-                rlie = lie;
-            }
+    // 计算宽高
+    for (i = 1; i <= n; i++) {
+        if (i * i >= n && n % i == 0) {
+            height = i;
+            width = n / i;
+            break;
         }
     }
-    // printf("rhang: %d\n", rhang);
-    // printf("rlie: %d\n", rlie);
-    
+    // 对原始数组进行递减排序
+    qsort(src, n, sizeof(int), compare);
+
+    int matrix[LEN]; // 矩阵
+    int w = width;   // 可变宽
+    int h = height;  // 可变高
+    int x = -1;      // 初始x位置
+    int y = 0;       // 初始y位置
+    int len = 0;     // 矩阵长度
+
+    while (w > 0 && h > 0) {
+        /*
+            在这里给大家分析下, 为什么每次需要保证h > 0, 或者
+            w > 0. 如果没有这两个保证, 将只能得到21分, 有2个  
+            测试点过不去.
+            假如现在只剩下最后一行, 假设高为1, 宽为6, 这个时候
+            我们走到终点, 整个矩阵遍历就结束了, 但此时的下一步
+            往下走也就是高减去1为0, 而宽还是6. 因为高为0了, 所以
+            向下不会走也就是不动, 但是再接着往左走, 这个时候因为  
+            宽不为0, 还会接着遍历, 这就出问题了. 因此每次都必须
+            要检测h > 0 或者 w > 0
+        */
+
+        // 向右
+        for (i = 0; i < w && h > 0; i++) {
+            x++;
+            matrix[x + y * width] = src[len++];
+        }
+        h--; // 这里也可以 if (h == 0) break; 下同理
+        // 向下
+        for (i = 0; i < h && w > 0; i++) {
+            y++;
+            matrix[x + y * width] = src[len++];
+        }
+        w--;
+        // 向左
+        for (i = 0; i < w && h > 0; i++) {
+            x--;
+            matrix[x + y * width] = src[len++];
+        }
+        h--;
+        // 向上
+        for (i = 0; i < h && w > 0; i++) {
+            y--;
+            matrix[x + y * width] = src[len++];
+        }
+        w--;
+    }
+    // 打印
+    char ch;
+    for (i = 0; i < len; i++) {
+        if ((i + 1) % width == 0) {
+            ch = '\n';
+        } else {
+            ch = ' ';
+        }
+        printf("%d%c", matrix[i], ch);
+    }
 
     return 0;
 }
 
-int move (int src[], int now, int lie) {
-    int next; // 移动后的位置
-    if (src[now + 1] != 0) {
-        src[now] = 0;
-        next = now + 1;
-    } else if(src[now + lie] != 0) {
-        src[now] = 0;
-        next = now + lie;
-    } else if (src[now - 1] != 0) {
-        src[now] = 0;
-        next = now - 1;
-    } else if (src[now - lie] != 0) {
-        src[now] = 0;
-        next = now - lie;
-    }
-    return next;
+int compare (const void *a, const void *b) {
+    int arg1 = *(int*)a;
+    int arg2 = *(int*)b;
+    return arg2 - arg1;
 }
