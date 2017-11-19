0a691b3239cd8e72bd54cbae876b74b4580f622c
Asheng <xys4k@qq.com>
2017-11-19 23:04:22 +0800

    完美体现了sprintf和sscanf的作用

--- src/1054.c
+++ src/1054.c
@@ -1,108 +1,45 @@
-/*
-本题的基本要求非常简单：给定N个实数，计算它们的平均值。但复杂的是有些输入数据可能是非法的。一个“合法”的输入是[-1000，1000]区间内的实数，并且最多精确到小数点后2位。当你计算平均值的时候，不能把那些非法的数据算在内。
-
-输入格式：
-
-输入第一行给出正整数N（<=100）。随后一行给出N个实数，数字间以一个空格分隔。
-
-输出格式：
-
-对每个非法输入，在一行中输出“ERROR: X is not a legal number”，其中X是输入。最后在一行中输出结果：“The average of K numbers is Y”，其中K是合法输入的个数，Y是它们的平均值，精确到小数点后2位。如果平均值无法计算，则用“Undefined”替换Y。如果K为1，则输出“The average of 1 number is Y”。
-
-输入样例1：
-7
-5 -3.2 aaa 9999 2.3.4 7.123 2.35
-输出样例1：
-ERROR: aaa is not a legal number
-ERROR: 9999 is not a legal number
-ERROR: 2.3.4 is not a legal number
-ERROR: 7.123 is not a legal number
-The average of 3 numbers is 1.38
-输入样例2：
-2
-aaa -9999
-输出样例2：
-ERROR: aaa is not a legal number
-ERROR: -9999 is not a legal number
-The average of 0 numbers is Undefined
-*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-int isLegalNum (char str[]);
+#include <string.h>
 
 int main (void) {
     int n;
-    int i;
-    char str[10000];
-    double now;
+    char src[100];  // 原始字符串
+    char dest[100]; // 保留2位小数的字符串 
     double sum = 0.0;
-    int numOfLegal = 0;
-    // [-1000，1000]
+    int count = 0;
+    double temp;
+    int flag;       // 标记是否合法, 0合法, 1不合法
+    int i;
+    int j;
+
     scanf("%d", &n);
     for (i = 1; i <= n; i++) {
-        scanf("%s", str);
-        if (isLegalNum(str)) {
-            sscanf(str, "%lf", &now);
-            if (now >= -1000.0 && now <= 1000.0) {
-                numOfLegal++;
-                sum += now;
-            } else {
-                printf("ERROR: %s is not a legal number\n", str);    
+        scanf("%s", src);
+        sscanf(src, "%lf", &temp);
+        sprintf(dest, "%.2f", temp);
+        flag = 0;
+        for (j = 0; j < strlen(src); j++) {
+            if (src[j] != dest[j]) {
+                flag = 1; // 不合法的数字
+                break;
             }
+        }
+        if (flag == 1 || temp < -1000 || temp > 1000) {
+            printf("ERROR: %s is not a legal number\n", src);
         } else {
-            printf("ERROR: %s is not a legal number\n", str);
+            count++;
+            sum += temp;
         }
     }
-    if (numOfLegal == 0) {
+    if (count == 0) {
         printf("The average of 0 numbers is Undefined");
+    } else if (count == 1) {
+        // MMP简直要崩溃, 这里number没有"s" (测试点2)
+        printf("The average of 1 number is %f", sum);
     } else {
-        printf("The average of %d numbers is %.2f", numOfLegal, sum / numOfLegal);
+        printf("The average of %d numbers is %.2f", count, sum / count);
     }
 
     return 0;
-}
-
-// 先判断是不是合法的小数
-// 1合法 0不合法
-int isLegalNum (char str[]) {
-    int len = strlen(str);
-    // int res;
-    int i;
-
-    if (str[0] == '.')  return 0;
-    if (str[0] == '-' && str[1] == '.')  return 0;
-    if (str[0] == '-') {
-        // res = 1;
-    } else if (str[0] >= '0' && str[0] <= '9') {
-        // res = 1;
-    } else {
-        return 0;
-    }
-    for (i = 1; i < len; i++) {
-        if (str[i] == '.') {
-            if (len - 1 == i + 2) {
-                if (
-                    (str[i + 1] >= '0' && str[i + 1] <= '9') &&
-                    (str[i + 2] >= '0' && str[i + 2] <= '9')
-                ) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } else if (len - 1 == i + 1) {
-                if (str[i + 1] >= '0' && str[i + 1] <= '9') {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } else {
-                return 0;
-            }
-        }
-        if (str[i] < '0' || str[i] > '9') {
-            return 0;
-        }
-    }
-    return 1;
 }
