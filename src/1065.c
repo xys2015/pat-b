7148500324f08ba4ea6d240eaff5807d46d959b7
Asheng <xys4k@qq.com>
2017-11-22 10:31:55 +0800

    完美用自己的方法搞定单身狗

--- src/1065.c
+++ src/1065.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define LEN 100000
+#define LEN 100010
 int main (void) {
-    int n; // 情侣对数
-    int nid[LEN]; // 互相存情侣号
-    int m; // 参加派对的总人数
-    int mid[LEN] = {0};   // 1有这个人 2落单
-    int single = 0; // 落单客人的总人数
+    int n;              // 情侣对数
+    int nid[LEN];       // 互相存情侣号
+    int m;              // 参加派对的总人数
+    int mid[LEN] = {0}; // 1有这个人 2落单
+    int single = 0;     // 落单客人的总人数
     int i;
     int tmp1;
     int tmp2;
@@ -17,14 +17,14 @@ int main (void) {
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         scanf("%d %d", &tmp1, &tmp2);
-        // printf("%d %d\n", tmp1, tmp2); exit(1);
+        // 互相存放自己的情侣号
         nid[tmp1] = tmp2;
         nid[tmp2] = tmp1;
     }
     scanf("%d", &m);
     for (i = 0; i < m; i++) {
         scanf("%d", &tmp1);
-        mid[tmp1] = 1;
+        mid[tmp1] = 1; // 标记为1, 这位客人参加了派对
     }
 
     for (i = 0; i < LEN; i++) {
@@ -47,10 +47,12 @@ int main (void) {
     for (i = 0; i < LEN; i++) {
         if (mid[i] == 2) {
             if (first == 1) {
-                printf("%5d", i);
+                // MD, 忘记了个5前面补零, 又耽误我
+                // 1小时+ (这是测试点3)
+                printf("%05d", i);
                 first = 0;
             } else {
-                printf(" %5d", i);
+                printf(" %05d", i);
             }
         }
     }
