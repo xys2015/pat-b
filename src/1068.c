893b82aeb6b335a746de09c4711fc739af11c277
Asheng <xys4k@qq.com>
2017-11-23 22:34:40 +0800

    被虐18小时+, 爽

--- src/1068.c
+++ src/1068.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <ctype.h>
+#include <stdlib.h>
+
 #define LEN 1010
-int isTol (int arr[][LEN], int n, int m, int tol);
+int isTol (int arr[][LEN], int i, int j, int n, int m, int tol);
 
 int repeat[1 << 24] = {0}; // = 2^24 = 16777216
 int arr[LEN][LEN];
+
 
 int main (void) {
     int m; // 列
@@ -20,15 +20,10 @@ int main (void) {
     int j;
 
     scanf("%d %d %d", &m, &n, &tol);
-    for (i = 0; i <= n + 1; i++) {
-        for (j = 0; j <= m + 1; j++) {
-            if (i == 0 || j == 0 || 
-                i == n + 1 || j == m + 1) {
-                arr[i][j] = 0;
-            } else {
-                scanf("%d", &arr[i][j]);
-                repeat[arr[i][j]]++;
-            }
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            scanf("%d", &arr[i][j]);
+            repeat[arr[i][j]]++;
         }
     }
 
@@ -37,9 +32,10 @@ int main (void) {
             // 先确定该点独一无二
             if (repeat[arr[i][j]] == 1) {
                 // 再确定满足色差超过tol的条件
-                if (isTol(arr, i, j, tol) == 1) {
+                if (isTol(arr, i, j, n, m, tol) == 1) {
                     x = j;
                     y = i;
+                    // printf("i:%d j:%d\n", i, j);
                     color = arr[i][j];
                     count++;
                     if (count == 2) break;
@@ -61,18 +57,19 @@ int main (void) {
 }
 
 // 是返回1, 不是返回0
-int isTol (int arr[][LEN], int n, int m, int tol) {
+// i行 j列 n最大行 m最大列
+int isTol (int arr[][LEN], int i, int j, int n, int m, int tol) {
     // 从左上角开始
     // n-1,m-1  n-1,m  n-1,m+1  n,m+1  n+1,m+1  n+1,m  n+1,m-1  n,m-1
     if (
-        (arr[n][m] - arr[n-1][m-1] > tol) && // 左上
-        (arr[n][m] - arr[n-1][m] > tol) &&   // 上
-        (arr[n][m] - arr[n-1][m+1] > tol) && // 右上
-        (arr[n][m] - arr[n][m+1] > tol) &&   // 右
-        (arr[n][m] - arr[n+1][m+1] > tol) && // 右下
-        (arr[n][m] - arr[n+1][m] > tol) &&   // 下
-        (arr[n][m] - arr[n+1][m-1] > tol) && // 左下
-        (arr[n][m] - arr[n][m-1] > tol)      // 左
+        (/* (i - 1 < 0 &&  j - 1 < 0) ? 1 :  */(abs(arr[i][j] - arr[i-1][j-1]) > tol)) && // 左上
+        (/* (i - 1 < 0) ? 1 :  */(abs(arr[i][j] - arr[i-1][j]) > tol)) &&   // 上
+        (/* (i - 1 < 0 && j + 1 > m) ? 1 :  */(abs(arr[i][j] - arr[i-1][j+1]) > tol)) && // 右上
+        (/* (j + 1 > m) ? 1 :  */(abs(arr[i][j] - arr[i][j+1]) > tol)) &&   // 右
+        (/* (i + 1 > n && j + 1 > m) ? 1 :  */(abs(arr[i][j] - arr[i+1][j+1]) > tol)) && // 右下
+        (/* (i + 1 > n) ? 1 :  */(abs(arr[i][j] - arr[i+1][j]) > tol)) &&   // 下
+        (/* (i + 1 > n && j - 1 < 0) ? 1 :  */(abs(arr[i][j] - arr[i+1][j-1]) > tol)) && // 左下
+        (/* (j - 1 < 0) ? 1 :  */(abs(arr[i][j] - arr[i][j-1]) > tol))      // 左
     ) {
         return 1;
     } else {
