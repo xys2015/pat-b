73c87ae19b0c6dba4ff6f81b868c89b8fda654df
Asheng <xys4k@qq.com>
2017-11-24 22:54:16 +0800

    非常nice, 自己的思想, 加上别人提供的错误矩阵的思路, 搞定

--- src/1073.c
+++ src/1073.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 struct Ques {
-    int qscore; // 满分
-    int qsnum;  // 选项个数
-    int qrnum; // 正确选项个数
-    int qawr[128]; // 所有正确选项
+    int qscore;     // 满分
+    int qsnum;      // 选项个数
+    int qrnum;      // 正确选项个数
+    int qawr[128];  // 所有正确选项
 };
 typedef struct Ques s_ques;
-struct Wrong {
-    int times; // 错误次数
-    int no;    // 题目编号
-    char ch;   // 选项号
-};
-typedef struct Wrong s_wrong;
 
 #define LEN 120
 int main (void) {
-    int snum; // 学生人数
-    int qnum; // 题目个数
-    s_ques ques[LEN]; // 每道题的信息
-    s_wrong wrong[500]; // 错误信息
+    int snum;           // 学生人数
+    int qnum;           // 题目个数
+    s_ques ques[LEN];   // 每道题的信息
+    int wrong[LEN][128] = {0}; // 错误信息矩阵
+    int wmax = 0;              // 最大错误次数
     char ch;
     int i;
     int j;
@@ -38,50 +30,37 @@ int main (void) {
         scanf("%d %d %d", &ques[i].qscore, &ques[i].qsnum, &ques[i].qrnum);
         for (j = 1; j <= ques[i].qrnum; j++) {
             scanf(" %c", &ch);
-            // printf("%c", ch);
             ques[i].qawr[(int)(ch)] = 1;
         }
-        // printf("\n");
     }
-    getchar();
+    getchar(); // 回车挡掉
 
-    // for (i = 1; i <= qnum; i++) {
-    //     for (j = 'a'; j <= 'e'; j++) {
-    //         printf("%c %d\n", j, ques[i].qawr[j]);
-    //     } printf("\n");
-    // } exit(1);
-
-    int len; // 初始是第1道题
-    int wlen = 0; // 错误信息长度
-    int mys; // 这个学生选了几个选项
-    char slt; // 选中选项
+    int len;   // 初始是第1道题
+    int mys;   // 这个学生选了几个选项
+    char slt;  // 选中选项
     for (i = 1; i <= snum; i++) {
-        double score = 0.0; // 每个学生的分数
+        double score = 0.0;  // 每个学生的初始分数
         len = 1;
+        // 一个 while 循环是一位学生答题的全部情况
         while ((ch = getchar()) != '\n') {
             if (ch == '(') {
-                int cawr[128] = {0};
+                int cawr[128] = {0}; // 用来记录该题答题情况, 有1, 无0
                 scanf("%d", &mys);
                 for (j = 1; j <= mys; j++) {
                     scanf(" %c", &slt);
-                    // printf("%c", slt);
-                    cawr[slt] = 1;
+                    cawr[(int)(slt)] = 1;
                 }
 
-                wrong[len].times = 0;
+                // 学生这个选项答了, 而正确答案没有, 相应错误+1
                 for (k = 'a'; k <= 'e'; k++) {
-                    // printf("%c %d\n", k, cawr[k]);
-                    // printf("%c %d\n", k, ques[len].qawr[k]);
                     if (cawr[k] != ques[len].qawr[k]) {
-                        wrong[wlen].times++;
-                        wrong[wlen].no = len;
-                        wrong[wlen].ch = k;
-                        wlen++;
+                        wrong[len][k]++;
                     }
+                    if (wrong[len][k] > wmax) wmax = wrong[len][k];
                 }
-                // printf("\n\n");
 
                 if (
+                    // 所有选项全部匹配, 为完全正确
                     cawr['a'] == ques[len].qawr['a'] &&
                     cawr['b'] == ques[len].qawr['b'] &&
                     cawr['c'] == ques[len].qawr['c'] &&
@@ -90,29 +69,35 @@ int main (void) {
                 ) {
                     score += (double)(ques[len].qscore);
                 } else if (
+                    // 这一项学生答了, 而正确答案没有这一项, 该题答错
                     (cawr['a'] == 1 && ques[len].qawr['a'] == 0) ||
                     (cawr['b'] == 1 && ques[len].qawr['b'] == 0) ||
                     (cawr['c'] == 1 && ques[len].qawr['c'] == 0) ||
                     (cawr['d'] == 1 && ques[len].qawr['d'] == 0) ||
                     (cawr['e'] == 1 && ques[len].qawr['e'] == 0) 
                 ) {
-                    // 该题错误
+                    // 该题答错
                     // do nothing
                 } else {
-                    // 这里是部分正确
+                    // 部分正确
                     score += (double)(ques[len].qscore) / 2.0;
                 }
-                len++;
+                len++; // 进入到下一题
             }
         }
-        printf("%.1f\n", score);
+        printf("%.1f\n", score); // 打印这位同学的得分
     }
 
-    for (i = 0; i < wlen; i++) {
-        printf(
-            "i:%d times:%d no:%d ch:%c\n", 
-            i, wrong[i].times, wrong[i].no, wrong[i].ch
-        );
+    if (wmax == 0) {
+        printf("Too simple\n");
+    } else {
+        for (i = 1; i <= qnum; i++) {
+            for (j = 'a'; j <= 'e'; j++) {
+                if (wrong[i][j] == wmax) {
+                    printf("%d %d-%c\n", wrong[i][j], i, j);
+                }
+            }
+        }
     }
 
     return 0;
