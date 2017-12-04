c48d63294f4dd7849cba2c0939a93ec57ae5f8fe
Asheng <xys4k@qq.com>
2017-12-04 22:25:54 +0800

    今天真正见识了, 负负得正怎么用的

--- src/1034.c
+++ src/1034.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-long getgcd (long a, long b); 
-void prlong (long a, long b);
-void compute (long a1, long b1, long a2, long b2, char op);
+long getgcd(long a, long b);
+void printfrac(long a, long b);
 
 int main (void) {
-    long a1;
-    long b1;
-    long a2;
-    long b2;
+	long a1;
+	long b1;
+	long a2;
+	long b2;
+    char op[4] = {'+', '-', '*', '/'};
+	int i;
 
-	scanf("%ld/%ld %ld/%ld", &a1, &b1, &a2, &b2); 
-    compute(a1, b1, a2, b2, '+');
-    compute(a1, b1, a2, b2, '-');
-    compute(a1, b1, a2, b2, '*');
-    compute(a1, b1, a2, b2, '/');
+	scanf("%ld/%ld %ld/%ld", &a1, &b1, &a2, &b2);
+    for(i = 0; i < 4; i++) {
+        printfrac(a1, b1);
+		printf(" %c ", op[i]);
+        printfrac(a2, b2);
+		printf(" = ");
+		// 我们使用的是long, 这里不用同分, 直接计算
+        switch (op[i]) {
+            case '+':
+				printfrac(a1 * b2 + a2 * b1, b1 * b2); 
+				break;
+            case '-':
+				printfrac(a1 * b2 - a2 * b1, b1 * b2);
+				break;
+            case '*':
+				printfrac(a1 * a2, b1 * b2);
+				break;
+            case '/':
+				printfrac(a1 * b2, b1 * a2);
+				break;
+        }
+        printf("\n");
+    }
+
     return 0;
 }
 
+
 long getgcd (long a, long b) {
-    long r;
-    while((r = a % b))
-    {
+	// 用其它求gcd的方法, 很可能导致
+	// 后两个测试点超时, 原因不明
+	long r;
+    while ((r = a % b)) {
         a = b;
         b = r;
 	}
@@ -32,72 +51,38 @@ long getgcd (long a, long b) {
     return b;
 }
 
-void print (long a, long b) {
-	if (a == 0) {
-		printf("0");
+
+void printfrac (long a, long b) {
+	/*
+		这里的a, b有多种情况, 正正, 正负, 负负都有可能
+		再下面判断符号时, 充分利用了负负的正的思想
+	*/
+    if (b == 0) {
+		printf("Inf");
 		return;
 	}
-	
-	long gcd = getgcd(a, b);
-    a = a / gcd;
-    b = b / gcd;
-    if (a < 0) printf("(");
-    if (b == 1) {
-		printf("%ld", a);
-	} else if (b > abs(a)) {
-		printf("%ld/%ld", a, b);
+
+    int sign = 1;
+	long gcd;
+    if (a < 0) {
+		a = -a;
+		sign = sign * -1;
+	}
+    if (b < 0) {
+		b = -b;
+		sign = sign * -1;
+	}
+	gcd = getgcd(a, b);
+	a = a / gcd;
+	b = b / gcd;
+
+    if (sign == -1) printf("(-");
+	if (b == 1) {
+		printf("%ld", a); 	// 整数
+	} else if (a > b) {
+		printf("%ld %ld/%ld", a / b, a % b, b);
 	} else {
-		printf("%ld %ld/%ld", a / b, abs(a) % b, b);
+		printf("%ld/%ld", a, b); // a < b
 	}
-    if(a < 0) printf(")");
+	if (sign == -1) printf(")");
 }
-
-void compute (long a1, long b1, long a2, long b2, char op) {
-    long a;
-    long b;
-//    long gcd;
-//    long lcm;
-
-    // 化简
-//    gcd = getgcd(a1, b1);
-//    a1 = a1 / gcd;
-//    b1 = b1 / gcd;
-//    gcd = getgcd(a2, b2);
-//    a2 = a2 / gcd;
-//    b2 = b2 / gcd;
-//    lcm = (b1 * b2) / getgcd(b1, b2);
-    
-    // 先打印, 避免夜长梦多
-    print(a1, b1);
-    printf(" %c ", op);
-    print(a2, b2);
-    printf(" = ");
-    
-    if (op == '+') {
-        a = a1 * b2 + a2 * b1;
-        b = b1 * b2;
-    } else if (op == '-') {
-        a = a1 * b2 - a2 * b1;
-        b = b1 * b2;
-    } else if (op == '*') {
-		a = a1 * a2;
-		b = b1 * b2;
-	} else if (op == '/') {
-		a = a1 * b2;
-		b = b1 * a2;
-		if (b < 0) {
-			b = -b;
-			a = -a;
-		}
-//		printf("a: %d\n", a);
-//		printf("b: %d\n", b);
-	}
-	
-    if (b == 0 && op == '/') {
-        printf("Inf");
-    } else {
-        print(a, b);
-    }
-    printf("\n");
-}
-
